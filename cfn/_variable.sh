d6c790be72605772e2280a6979e296bd45afeef7
Toshiaki Aoike <aoike@cloudpack.jp>
2019-02-14 09:39:05 +0900

    first commit

--- cfn/_variable.sh
+++ cfn/_variable.sh
@@ -4,7 +4,7 @@ export NAME="cicd-f"
 export SYS_NAME=$NAME-$STAGE
 export BRANCH="master"
 export AWS_DEFAULT_REGION="ap-northeast-1"
-export AQUA_TOKEN=""
+export AQUA_TOKEN="Mzg4ZTRkMGI1NTQ3"
 export USE_PROFILE=""
 if [ -n "$USE_PROFILE" ]; then
     export AWS_DEFAULT_PROFILE=$3
